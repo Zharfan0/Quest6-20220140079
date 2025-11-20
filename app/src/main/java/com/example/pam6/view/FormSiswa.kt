ef6aad199e411c43854c0d5945b8f85d3ed87c01
Zharfan <zharfanwidyadana@gmail.com>
2025-11-20 15:51:14 +0700

    mendeklarasikan variabel-variabel pada FormSiswa (FormSiswa.kt)

--- app/src/main/java/com/example/pam6/view/FormSiswa.kt
+++ app/src/main/java/com/example/pam6/view/FormSiswa.kt
@@ -2,9 +2,13 @@ package com.example.pam6.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormSiswa(
-
+    //edit 1 ; parameter pilihan JK dan onSubmitButtonClicked
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
 ){}
