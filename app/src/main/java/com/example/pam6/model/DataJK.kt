7cfee28d21a8672c02b4712d502ef093ae0d34e6
Zharfan <zharfanwidyadana@gmail.com>
2025-11-20 15:44:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pam6/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.pam6.model
+
+import androidx.compose.ui.R
+
+object DataJK {
+    val JenisK= listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
---
+++ app/src/main/java/com/example/pam6/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.pam6.viewmodel
+
+import android.R
+import androidx.lifecycle.ViewModel
+import com.example.pam6.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls: MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(ls[0], gender = ls[1], alamat = ls[2])
+        }
+    }
+}
