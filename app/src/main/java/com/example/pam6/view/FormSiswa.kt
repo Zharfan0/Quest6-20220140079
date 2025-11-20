e358d231cd2b38397e85976f4cd7ddba4638ae3a
Zharfan <zharfanwidyadana@gmail.com>
2025-11-20 15:49:26 +0700

    membuat fungsi FormSiswa (FormSiswa.kt)

--- app/src/main/java/com/example/pam6/view/FormSiswa.kt
+++ app/src/main/java/com/example/pam6/view/FormSiswa.kt
@@ -1,2 +1,10 @@
 package com.example.pam6.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+
+){}
